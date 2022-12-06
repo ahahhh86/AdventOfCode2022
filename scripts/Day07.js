31774bd09282c811114da2b82dbb1c92d5e1205d
ahahhh86 <92790113+ahahhh86@users.noreply.github.com>
2022-12-06 12:03:55 +0100

    Create Day07.js

---
+++ scripts/Day07.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+
+function solveDay07() {
+	document.getElementById('solution_d07_p1').innerHTML = "Day07Test";
+	document.getElementById('solution_d07_p2').innerHTML = "Day07Test";
+}
