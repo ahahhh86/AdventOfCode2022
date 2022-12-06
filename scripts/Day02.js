4e7c82d5f127c767ab5a9440491f68ce59d244de
ahahhh86 <92790113+ahahhh86@users.noreply.github.com>
2022-12-06 13:25:40 +0100

    Update Day02.js

--- scripts/Day02.js
+++ scripts/Day02.js
@@ -1,6 +1,7 @@
 'use strict';
 
 
+
 /*
 	--- Day 2: Rock Paper Scissors ---
 	The Elves begin to set up camp on the beach. To decide whose tent gets to be closest to the snack storage, a giant Rock Paper Scissors tournament is
